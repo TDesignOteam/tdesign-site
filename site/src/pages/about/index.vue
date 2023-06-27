2f434d5c2c2f7246f5740d08abf8ce58fff3646a
Uyarn <uyarnchen@gmail.com>
2023-06-27 21:04:03 +0800

    fix: fix en docs router

--- site/src/pages/about/index.vue
+++ site/src/pages/about/index.vue
@@ -8,16 +8,19 @@
 
 <script>
 import siteConfig from '../../site.config'
+import siteEnConfig from '../../site-en.config'
+
 import pageLoadMixin from '../mixins/page-load.js'
 
 const { docs: aboutDocs } = JSON.parse(JSON.stringify(siteConfig.about).replace(/component:.+/g, ''))
+const { docs: aboutEnDocs } = JSON.parse(JSON.stringify(siteEnConfig.about).replace(/component:.+/g, ''))
 
 export default {
   mixins: [pageLoadMixin],
 
   computed: {
     asideList () {
-      if (this.$route.path.includes('/about')) return aboutDocs
+      if (this.$route.path.includes('en')) return aboutEnDocs
       return aboutDocs
     }
   },
